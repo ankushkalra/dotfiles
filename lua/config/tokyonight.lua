7390b7d88dfe1a8427030cf0810c8289f9f8c79d
Ankush Kalra <ankushkalra2@gmail.com>
2025-11-19 19:42:41 +0530

    set cursor color for tokyonight

--- lua/config/tokyonight.lua
+++ lua/config/tokyonight.lua
@@ -1,1 +1,8 @@
+vim.cmd [[set background=dark ]]
 vim.cmd [[colorscheme tokyonight]]
+
+vim.cmd([[
+  highlight NormalCursor guibg=#777777
+  highlight InsertCursor guibg=#a9b665
+  set guicursor=n:block-NormalCursor,i:ver25-InsertCursor
+]])
