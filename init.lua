7a5943902f795a001e0438b6d9160e784a9a05eb
Ankush Kalra <ankushkalra2@gmail.com>
2025-11-19 13:50:46 +0530

    moved vscode config to its file

--- init.lua
+++ init.lua
@@ -7,38 +7,4 @@ require("config.lsp")
 require("config.term")
 require("config.conform")
 require("config.vim-fugitive")
-
--- Lua
-vim.o.termguicolors = true -- Enable true color support
-
-local c = require("vscode.colors").get_colors()
-require("vscode").setup({
-  -- Alternatively set style in setup
-  -- style = 'light'
-
-  -- Enable transparent background
-  transparent = true,
-
-  -- Enable italic comment
-  italic_comments = true,
-
-  -- Underline `@markup.link.*` variants
-  underline_links = true,
-
-  -- Disable nvim-tree background color
-  disable_nvimtree_bg = true,
-
-  -- Apply theme colors to terminal
-  terminal_colors = true,
-
-  -- Override colors (see ./lua/vscode/colors.lua)
-  color_overrides = {},
-
-  -- Override highlight groups (see ./lua/vscode/theme.lua)
-  group_overrides = {
-    -- this supports the same val table as vim.api.nvim_set_hl
-    -- use colors from this colorscheme by requiring vscode.colors!
-    Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-  },
-})
-vim.cmd.colorscheme "vscode"
+require("config.vscode")
--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -1,42 +1,16 @@
--- Lua:
--- For dark theme (neovim's default)
-vim.o.background = "dark"
--- For light theme
-vim.o.background = "light"
+-- Lua
+vim.o.termguicolors = true -- Enable true color support
 
 local c = require("vscode.colors").get_colors()
 require("vscode").setup({
-	-- Alternatively set style in setup
-	-- style = "light",
-
-	-- Enable transparent background
-	transparent = true,
-
-	-- Enable italic comment
-	italic_comments = true,
-
-	-- Underline `@markup.link.*` variants
-	underline_links = true,
-
-	-- Disable nvim-tree background color
-	disable_nvimtree_bg = true,
-
-	-- Apply theme colors to terminal
-	terminal_colors = true,
-
-	-- Override colors (see ./lua/vscode/colors.lua)
-	color_overrides = {
-		vscLineNumber = "#FFFFFF",
-	},
-
-	-- Override highlight groups (see ./lua/vscode/theme.lua)
-	group_overrides = {
-		-- this supports the same val table as vim.api.nvim_set_hl
-		-- use colors from this colorscheme by requiring vscode.colors!
-		Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-	},
+  transparent = true,
+  italic_comments = true,
+  underline_links = true,
+  disable_nvimtree_bg = true,
+  terminal_colors = true,
+  color_overrides = {},
+  group_overrides = {
+    Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+  },
 })
--- require('vscode').load()
-
--- load the theme without affecting devicon colors.
-vim.cmd.colorscheme("vscode")
+vim.cmd.colorscheme "vscode"
