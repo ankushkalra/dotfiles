2328e0624670ddbd49a77ed4986aa4af0e41dd4c
Ankush Kalra <ankushkalra2@gmail.com>
2026-05-24 01:16:38 +0530

    focus current buffer in nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,6 +6,11 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      update_focused_file = {
+        enable = true,      -- Automatically focus the file in the tree
+        update_cwd = false, -- Change the tree's root to match the file's dir
+      },
+    }
   end,
 }
