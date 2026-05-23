6935ffb110c13e3aeaaf167c2ef8d107297e2d8b
Ankush Kalra <ankushkalra2@gmail.com>
2026-05-24 01:16:30 +0530

    retain cursor position in previously opened file

--- lua/config/editor.lua
+++ lua/config/editor.lua
@@ -7,3 +7,13 @@ vim.opt.signcolumn = "yes"
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true -- Recommended: Use spaces instead of tabs
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
