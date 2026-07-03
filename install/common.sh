904c3aaf5a32241636eb743663d403d4b2b8387f
Ankush Kalra <ankushkalra2@gmail.com>
2026-07-03 14:21:10 +0530

    moved nvim config to nvim folder

--- install/common.sh
+++ install/common.sh
@@ -2,4 +2,4 @@ ln -sf "$PWD"/.zshrc "$HOME"/.zshrc
 ln -sf "$PWD"/.p10k.zsh "$HOME"/.p10k.zsh
 ln -sf "$PWD"/.gitconfig "$HOME"/.gitconfig
 mkdir -p "$HOME"/.config
-ln -sfn "$PWD"/.config/nvim "$HOME"/.config/nvim
+ln -sfn "$PWD"/nvim "$HOME"/.config/nvim
